4787d9d3c04df09dc94a90a1008d2daacb5d44e8
drafire <hyljljlj@163.com>
2019-02-19 22:11:27 +0800

    整合了redis 的操作，实现redis的增删改查

--- src/main/java/com/drafire/spring_boot/controller/RedisController.java
+++ src/main/java/com/drafire/spring_boot/controller/RedisController.java
@@ -1,19 +1,21 @@
 package com.drafire.spring_boot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RedisController {
+
+
     @Autowired
-    private StringRedisTemplate redisTemplate;
+    private RedisTemplate redisTemplate;
 
     @GetMapping(value = "/redis/get/{key}")
     public Object getRedis(@PathVariable String key){
-        Object object= redisTemplate.opsForValue().get(key);
+        Object object= redisTemplate.opsForValue().get(key); //注意，如果这里是字符串，并且字符串有空格的话，则需要做额外处理
         return object;
     }
 
@@ -28,4 +30,5 @@ public class RedisController {
         redisTemplate.delete(key);
         return "ok";
     }
+
 }
